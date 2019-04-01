fda82e43ceca504aebd35d2d8e8030b0c5308c53
milozou <zouqixian@gmail.com>
2019-04-01 10:10:37 +0800

    Add files via upload

---
+++ 01/wujiaoxing.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
